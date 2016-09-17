43452383368c8724f690253ea9d1ab89904c1e25
Keean Schupke <keean@fry-it.com>
2016-09-17 17:35:09 +0100

    generate literal

--- src/traitscript.js
+++ src/traitscript.js
@@ -1,14 +1,20 @@
 module.exports = (function TSModule() {
 
-    console.log('TraitScript compiler (C)2016 Keean Schupke')
+console.log('TraitScript compiler (C)2016 Keean Schupke')
 
-    var exports = function TraitScript(ast) {
-        this.ast = ast;
-    }
+var exports = function TraitScript(ast) {
+    this.ast = ast;
+}
 
-    exports.prototype.generate = function generate() {
+exports.prototype.generate = function generate() {
+    var ast = this.ast;
+
+    if (ast.lit) {
+        return ast.lit;
+    } else {
         return "";
     }
+}
 
-    return exports;
+return exports;
 })()
--- test/generate-test.js
+++ test/generate-test.js
@@ -3,8 +3,14 @@ var expect = chai.expect;
 var TraitScript = require('../src/traitscript.js');
 
 describe('Generator', function() {
-    it('generate() should produce an empty JS file when passed an empty AST', function() {
-        var compiler = new TraitScript();
-        expect(compiler.generate({})).to.equal("");
+    it('given an empty AST, produce an empty JS output', function() {
+        var compiler = new TraitScript({});
+        expect(compiler.generate()).to.equal("");
+    });
+
+    it('given a literal AST node, produce the JS for it', function() {
+        var compiler = new TraitScript({'lit' : '1'});
+        expect(compiler.generate()).to.equal("1");
     });
 });
+
