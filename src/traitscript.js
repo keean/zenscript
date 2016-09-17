719811dd8fbb551d72b5c4b613f8c81ecb0d3867
Keean Schupke <keean@fry-it.com>
2016-09-17 22:38:25 +0100

    rearrange to allow statements in function definitions

--- src/traitscript.js
+++ src/traitscript.js
@@ -2,6 +2,10 @@ module.exports = (function TSModule() {
 
 console.log('TraitScript compiler (C)2016 Keean Schupke')
 
+function gen_fn(ast) {
+    return 'function ' + ast.fn + '(' + ast.args.join(',') + '){' + gen_blk(ast.body) + '}';
+}
+
 function gen_exp(ast) {
     if (ast.lit) {
         return ast.lit;
@@ -9,22 +13,32 @@ function gen_exp(ast) {
         return ast.var;
     } else if (ast.app) {
         return ast.app + '(' + ast.args.map(gen_exp).join(',') + ')';
+    } else if (ast.fn) {
+        return gen_fn(ast);
     } else {
         return '';
     }
 }
 
-function gen_fn(ast) {
-    return 'function ' + ast.fn + '(' + ast.args.join(',') + '){return ' + gen_exp(ast.body) + ';}';
+function gen_stmt(ast) {
+    if (ast.fn) {
+        return gen_fn(ast);
+    } else if (ast.decl) {
+        return 'var ' + ast.decl + '=' + gen_exp(ast.exp) + ';';
+    } else if (ast.ass) {
+        return ast.ass + '=' + gen_exp(ast.exp) + ';';
+    } else if (ast.rtn) {
+        return 'return ' + gen_exp(ast.rtn) + ';';
+    } else {
+        return gen_exp(ast) + ';';
+    }
 }
 
 function gen_blk(ast) {
     if (ast.blk) {
-        return ast.def.map(gen_fn).join('') + ast.blk.map(gen_exp).join(';') + ';';
-    } else if (ast.fn) {
-        return gen_fn(ast);
+        return ast.blk.map(gen_stmt).join('');
     } else {
-        return gen_exp(ast);
+        return gen_stmt(ast);
     }
 }
 
--- test/generate-test.js
+++ test/generate-test.js
@@ -5,17 +5,17 @@ var TraitScript = require('../src/traitscript.js');
 describe('Generator', function() {
     it('given an empty AST, produce an empty JS output', function() {
         var compiler = new TraitScript({});
-        expect(compiler.generate()).to.equal("");
+        expect(compiler.generate()).to.equal(";");
     });
 
     it('given a literal AST node, produce the JS for it', function() {
         var compiler = new TraitScript({'lit' : '1'});
-        expect(compiler.generate()).to.equal("1");
+        expect(compiler.generate()).to.equal("1;");
     });
 
     it('given a variable AST node, produce the JS for it', function() {
         var compiler = new TraitScript({'var' : 'x'});
-        expect(compiler.generate()).to.equal("x");
+        expect(compiler.generate()).to.equal("x;");
     });
 
     it('given a simple application AST tree, produce the JS for it', function() {
@@ -23,7 +23,7 @@ describe('Generator', function() {
             'app' : 'f', 
             'args' : [{'var' : 'x'}, {'var' : 'y'}]
         })
-        expect(compiler.generate()).to.equal('f(x,y)');
+        expect(compiler.generate()).to.equal('f(x,y);');
     });
 
     it('given a simple function AST tree, produce the JS for it', function() {
@@ -31,7 +31,7 @@ describe('Generator', function() {
             'fn' : 'f',
             'args' : ['x', 'y'],
             'body' : {
-                'var' : 'x'
+                'rtn' : {'var' : 'x'}
             }
         });
         expect(compiler.generate()).to.equal('function f(x,y){return x;}');
@@ -39,10 +39,8 @@ describe('Generator', function() {
 
     it('given a stament block in the AST tree, produce the JS for it', function() {
         var compiler = new TraitScript({
-            'def' : [
-                {'fn' : 'f', 'args' : ['x', 'y'], 'body' : {'var' : 'x'}}
-            ],
             'blk' : [
+                {'fn' : 'f', 'args' : ['x', 'y'], 'body' : {'rtn' : {'var' : 'x'}}},
                 {'app' : 'f', 'args' : [{'lit' : '1'}, {'lit' : '2'}]}
             ]
         });
@@ -51,10 +49,8 @@ describe('Generator', function() {
 
     it('given a stament block in the AST tree, run the generated JS', function() {
         var compiler = new TraitScript({
-            'def' : [
-                {'fn' : 'f', 'args' : ['x', 'y'], 'body' : {'var' : 'x'}},
-            ],
             'blk' : [
+                {'fn' : 'f', 'args' : ['x', 'y'], 'body' : {'rtn' : {'var' : 'x'}}},
                 {'app' : 'f', 'args' : [{'lit' : '1'}, {'lit' : '2'}]}
             ]
         });
