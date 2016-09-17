2874c091b2015b470f5adc1fb177c469fb33d7df
Keean Schupke <keean@fry-it.com>
2016-09-17 22:08:47 +0100

    split generator into expression, function, and block

--- src/traitscript.js
+++ src/traitscript.js
@@ -2,19 +2,29 @@ module.exports = (function TSModule() {
 
 console.log('TraitScript compiler (C)2016 Keean Schupke')
 
-function gen(ast) {
+function gen_exp(ast) {
     if (ast.lit) {
         return ast.lit;
     } else if (ast.var) {
         return ast.var;
     } else if (ast.app) {
-        return ast.app + '(' + ast.args.map(gen).join(',') + ')';
-    } else if (ast.fn) {
-        return 'function ' + ast.fn + '(' + ast.args.join(',') + '){return ' + gen(ast.body) + ';}';
-    } else if (ast.blk) {
-        return ast.def.map(gen).join('') + ast.blk.map(gen).join(';') + ';';
+        return ast.app + '(' + ast.args.map(gen_exp).join(',') + ')';
     } else {
         return '';
+    }
+}
+
+function gen_fn(ast) {
+    return 'function ' + ast.fn + '(' + ast.args.join(',') + '){return ' + gen_exp(ast.body) + ';}';
+}
+
+function gen_blk(ast) {
+    if (ast.blk) {
+        return ast.def.map(gen_fn).join('') + ast.blk.map(gen_exp).join(';') + ';';
+    } else if (ast.fn) {
+        return gen_fn(ast);
+    } else {
+        return gen_exp(ast);
     }
 }
 
@@ -23,7 +33,7 @@ var exports = function TraitScript(ast) {
 }
 
 exports.prototype.generate = function generate() {
-    return gen(this.ast);
+    return gen_blk(this.ast);
 }
 
 return exports;
