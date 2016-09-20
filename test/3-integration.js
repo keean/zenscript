9d73c0e5cdc01815e352e04279fb22feefcd7931
Keean Schupke <keean@fry-it.com>
2016-09-20 17:10:35 +0100

    add parse, generate, evaluete integration test

--- test/3-integration.js
+++ test/3-integration.js
@@ -4,8 +4,14 @@ var parse = require('../src/parse.js');
 var generate = require('../src/generate.js');
 
 describe('Integration', function() {
-    it('parse simple assignment, and expression, then generate and eval', function() {
+    it('parse simple assignment, and expression, then generate', function() {
         expect(generate(parse('x = 3\nx'))).to.equal('x=3;x;');
+    });
+
+    it('parse simple assignment, and expression, then generate and eval', function() {
+        /* jshint evil:true */
+        expect(eval(generate(parse('x = 3\nx')))).to.equal(3);
+        /* jshint evil:false */
     });
 });
 
