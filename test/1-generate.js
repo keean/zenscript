ae8a1b0b7a0284527fb8157d25bbf78b59c9fb97
Keean Schupke <keean@fry-it.com>
2016-10-03 10:42:26 +0100

    fix generator unit tests

--- test/1-generate.js
+++ test/1-generate.js
@@ -1,64 +1,55 @@
 var chai = require('chai')
 var expect = chai.expect
+var AST = require('../src/ast.js')
 var generate = require('../src/generate.js')
 
 describe('Generate', () => {
     it('given an empty AST, produce an empty JS output', () => {
-        expect(generate({})).to.equal('')
+        expect((new AST.Block([])).generate()).to.equal('')
     })
 
     it('given a literal AST node, produce the JS for it', () => {
-        expect(generate({'lit' : '1'})).to.equal('1;')
+        expect((new AST.Literal_Int(1)).generate()).to.equal('1')
     })
 
     it('given a variable AST node, produce the JS for it', () => {
-        expect(generate({'var' : 'x'})).to.equal('x;')
+        expect((new AST.Variable('x')).generate()).to.equal('x')
     })
 
     it('given a simple application AST tree, produce the JS for it', () => {
-        expect(generate({
-            'app' : 'f', 
-            'args' : [{'var' : 'x'}, {'var' : 'y'}]
-        })).to.equal('f(x,y);')
+        expect((new AST.Application('f', [
+            new AST.Variable('x'),
+            new AST.Variable('y')
+        ])).generate()).to.equal('f(x,y)')
     })
 
     it('given a simple function AST tree, produce the JS for it', () => {
-        expect(generate({
-            'fn' : 'f',
-            'args' : ['x', 'y'],
-            'body' : {
-                'rtn' : {'var' : 'x'}
-            }
-        })).to.equal('function f(x,y){return x;}')
+        expect((
+            new AST.Fn('f', ['x', 'y'], new AST.Return(new AST.Variable('x')))
+        ).generate()).to.equal('function f(x,y){return x;}')
     })
 
     it('given a stament block in the AST tree, produce the JS for it', () => {
-        expect(generate({
-            'blk' : [
-                {'fn' : 'f', 'args' : ['x', 'y'], 'body' : {'rtn' : {'var' : 'x'}}},
-                {'app' : 'f', 'args' : [{'lit' : '1'}, {'lit' : '2'}]}
-            ]
-        })).to.equal('function f(x,y){return x;}f(1,2);')
+        expect((new AST.Block([
+            new AST.Fn('f', ['x', 'y'], new AST.Return(new AST.Variable('x'))),
+            new AST.Application('f', [new AST.Literal_Int(1), new AST.Literal_Int(2)])
+        ])).generate()).to.equal('function f(x,y){return x;}f(1,2)')
     })
 
     it('given a stament block in the AST tree, run the generated JS', () => {
         /* jshint evil:true */
-        expect(eval(generate({
-            'blk' : [
-                {'fn' : 'f', 'args' : ['x', 'y'], 'body' : {'rtn' : {'var' : 'x'}}},
-                {'app' : 'f', 'args' : [{'lit' : '1'}, {'lit' : '2'}]}
-            ]
-        }))).to.equal(1)
+        expect(eval((new AST.Block([
+            new AST.Fn('f', ['x', 'y'], new AST.Return(new AST.Variable('x'))),
+            new AST.Application('f', [new AST.Literal_Int(1), new AST.Literal_Int(2)])
+        ])).generate())).to.equal(1)
         /* jshint evil:false */
     })
 
     it('given a function definition and application, generate the AST', () => {
-        expect(generate({
-            'blk' : [
-                {'ass' : 'id', 'exp' : {'fn' : 'id', 'args' : ['x'], 'body' : {'rtn' : {'var' : 'x'}}}},
-                {'app' : 'id', 'args' : [{'lit' : 42}]}
-             ]
-        })).to.equal('id=function id(x){return x;};id(42);')
+        expect((new AST.Block([
+            new AST.Assignment('id', new AST.Fn('id', ['x'], new AST.Return(new AST.Variable('x')))),
+            new AST.Application('id', [new AST.Literal_Int(42)])
+        ])).generate()).to.equal('id=function id(x){return x;};id(42)')
     })
 })
 
