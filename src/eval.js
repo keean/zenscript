4e33d14e9a73aaf81b9c7a9a884c53d5b3355f0a
Keean Schupke <keean@fry-it.com>
2016-10-16 19:56:33 +0100

    update eval to use const

--- src/eval.js
+++ src/eval.js
@@ -1,27 +1,27 @@
-var eval_file = (() => {
-    "use strict"
+const eval_file = (() => {
+   "use strict"
 
-    var fs = require('fs')
-    var path = require('path')
+   const fs = require('fs')
+   const path = require('path')
 
-    return (source) => {
-        if (path.extname(source) !== '.js') {
-            return console.log('filename: "' + source + '" does not have .js extension')
-        }
-        fs.readFile(source, 'utf8', (err, data) => {
-            if (err) {
-                return console.log(err)
-            }
+   return (source) => {
+      if (path.extname(source) !== '.js') {
+         return console.log('filename: "' + source + '" does not have .js extension')
+      }
+      fs.readFile(source, 'utf8', (err, data) => {
+         if (err) {
+            return console.log(err)
+         }
 
-            /* jshint evil:true */
-            console.log(eval(data))
-            /* jshint evil:false */
-        })
-    }
+         /* jshint evil:true */
+         console.log(eval(data))
+         /* jshint evil:false */
+      })
+   }
 })()
 
 if (process.argv.length != 3) {
-    return console.log('Please specify file')
+   return console.log('Please specify file')
 }
 
 eval_file(process.argv[2])
