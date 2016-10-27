558118280816d519852836205229af99d0dbdc9d
Keean Schupke <keean@fry-it.com>
2016-10-27 07:38:49 +0100

    fix tabs

--- test/3-integration.js
+++ test/3-integration.js
@@ -6,24 +6,24 @@ const Generate = require('../src/generate.js')
 generate = new Generate()
 
 describe('Integration', () => {
-    it('parse simple assignment, and expression, then generate', () => {
-        expect(generate.minimal(parse.program('let x = 3\nx').value)).to.equal('var x=3;x')
-    })
+   it('parse simple assignment, and expression, then generate', () => {
+      expect(generate.minimal(parse.program('let x = 3\nx').value)).to.equal('var x=3;x')
+   })
 
-    it('parse simple assignment, and expression, then generate and eval', () => {
-        /* jshint evil:true */
-        expect(eval(generate.minimal(parse.program('let x = 3\nx').value))).to.equal(3)
-        /* jshint evil:false */
-    })
+   it('parse simple assignment, and expression, then generate and eval', () => {
+      /* jshint evil:true */
+      expect(eval(generate.minimal(parse.program('let x = 3\nx').value))).to.equal(3)
+      /* jshint evil:false */
+   })
 
-    it('parse function assignment, and expression, then generate', () => {
-        expect(generate.minimal(parse.program('let id = id(x) => x').value)).to.equal('var id=function id(x){return x;};')
-    })
+   it('parse function assignment, and expression, then generate', () => {
+      expect(generate.minimal(parse.program('let id = id(x) => x').value)).to.equal('var id=function id(x){return x;};')
+   })
 
-    it('parse function assignment, and application, then generate', () => {
-        /* jshint evil:true */
-        expect(eval(generate.minimal(parse.program('let id = id(x) => x\nid(42)').value))).to.equal(42)
-        /* jshint evil:false */
-    })
+   it('parse function assignment, and application, then generate', () => {
+      /* jshint evil:true */
+      expect(eval(generate.minimal(parse.program('let id = id(x) => x\nid(42)').value))).to.equal(42)
+      /* jshint evil:false */
+   })
 })
 
