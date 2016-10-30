0a2bd37d0e591ebdfca3d747a8e3373f854c7938
Keean Schupke <keean@fry-it.com>
2016-10-30 16:37:30 +0000

    fix generate to use new name as variable

--- test/1-generate.js
+++ test/1-generate.js
@@ -27,13 +27,13 @@ describe('Generate', () => {
 
    it('given a simple function AST tree, produce the JS for it', () => {
       expect(generate.minimal(
-         new AST.Fn('f', ['x', 'y'], new AST.Return(new AST.Variable('x')))
+         new AST.Fn('f', [new AST.Variable('x'), new AST.Variable('y')], new AST.Return(new AST.Variable('x')))
       )).to.equal('function f(x,y){return x;}')
    })
 
    it('given a stament block in the AST tree, produce the JS for it', () => {
       expect(generate.minimal(new AST.Block([
-         new AST.Fn('f', ['x', 'y'], new AST.Return(new AST.Variable('x'))),
+         new AST.Fn('f', [new AST.Variable('x'), new AST.Variable('y')], new AST.Return(new AST.Variable('x'))),
          new AST.Application(new AST.Variable('f'), new AST.LiteralTuple([new AST.LiteralInt(1), new AST.LiteralInt(2)]))
       ]))).to.equal('function f(x,y){return x;}f(1,2)')
    })
@@ -41,7 +41,7 @@ describe('Generate', () => {
    it('given a stament block in the AST tree, run the generated JS', () => {
       /* jshint evil:true */
       expect(eval(generate.minimal(new AST.Block([
-         new AST.Fn('f', ['x', 'y'], new AST.Return(new AST.Variable('x'))),
+         new AST.Fn('f', [new AST.Variable('x'), new AST.Variable('y')], new AST.Return(new AST.Variable('x'))),
          new AST.Application(new AST.Variable('f'), new AST.LiteralTuple([new AST.LiteralInt(1), new AST.LiteralInt(2)]))
       ])))).to.equal(1)
       /* jshint evil:false */
@@ -49,7 +49,7 @@ describe('Generate', () => {
 
    it('given a function definition and application, generate the AST', () => {
       expect(generate.minimal(new AST.Block([
-         new AST.Assignment('id', new AST.Fn('id', ['x'], new AST.Return(new AST.Variable('x')))),
+         new AST.Assignment('id', new AST.Fn('id', [new AST.Variable('x')], new AST.Return(new AST.Variable('x')))),
          new AST.Application(new AST.Variable('id'), new AST.LiteralTuple([new AST.LiteralInt(42)]))
       ]))).to.equal('id=function id(x){return x;};id(42)')
    })
