6588bcd3a213a226402b017f921dc91952d623b9
Keean Schupke <keean@fry-it.com>
2016-11-04 19:28:01 +0000

    tidy up explain code

--- src/typing-explain.js
+++ src/typing-explain.js
@@ -6,10 +6,13 @@ const Show = require('../src/typing-show.js')
 
 const show = new Show()
 
+//----------------------------------------------------------------------------
+
 class Step {
-   constructor(id, text) {
+   constructor(id, node, type) {
       this.id = id
-      this.text = text
+      this.node = node
+      this.type = type
    }
 }
 
@@ -17,8 +20,22 @@ function last(xs) {
    return xs[xs.length - 1]
 }
 
+function flatten(xss) {
+   const r = []
+   for (let i = 0; i < xss.length; ++i) {
+      const xs = xss[i]
+      for (let j = 0; j < xs.length; ++j) {
+         r.push(xs[j])
+      }
+   }
+   return r
+}
+
+//----------------------------------------------------------------------------
+
 AST.LiteralInt.prototype.explain = function(cxt) {
-   return [new Step(cxt.id++, '[int ' + this.value.toString() + ']\t' + show.typing(this.typing) + '\n')]
+   return [new Step(cxt.id++, '[int ' + this.value.toString() + ']',
+      show.typing(this.typing))]
 }
 
 /*
@@ -31,28 +48,29 @@ AST.LiteralArray.prototype.explain = function() {
 
 AST.LiteralTuple.prototype.explain = function(cxt) {
    const ps = this.expressions.map((x) => x.explain(cxt)) 
-   const t = [].concat.apply([], ps)
-   t.push(new Step(cxt.id++, '[tpl ' + ps.map((x) => last(x).id).join(' ') + ']\t' +
-      show.typing(this.typing) + '\n'))
+   const t = flatten(ps)
+   t.push(new Step(cxt.id++, '[tpl ' + ps.map((x) => last(x).id).join(' ') + ']',
+      show.typing(this.typing)))
    return t 
 }
 
 AST.Variable.prototype.explain = function(cxt) {
-   return [new Step(cxt.id++, '[var ' + this.name + ']\t' + show.typing(this.typing) + '\n')]
+   return [new Step(cxt.id++, '[var ' + this.name + ']', show.typing(this.typing))]
 }
 
 AST.Application.prototype.explain = function(cxt) {
    const f = this.fun.explain(cxt)
    const a = this.arg.explain(cxt)
    const b = f.concat(a)
-   b.push(new Step(cxt.id++, '[app ' + last(f).id + " " + last(a).id + ']\t' + show.typing(this.typing) + '\n'))
+   b.push(new Step(cxt.id++, '[app ' + last(f).id + " " + last(a).id + ']',
+      show.typing(this.typing)))
    return b 
 }
 
 AST.Fn.prototype.explain = function(cxt) {
    const body = this.body.explain(cxt)
    body.push(new Step(cxt.id++, '[fun ' + (this.name ? this.name + ' ' : '') + 
-      this.args.map((x) => x.name).join(' ') + ']\t' + show.typing(this.typing) + '\n'))
+      this.args.map((x) => x.name).join(' ') + ']', show.typing(this.typing)))
    return body
 }
 
@@ -60,8 +78,8 @@ AST.Declaration.prototype.explain = function(cxt) {
    const v = this.variable.explain(cxt)
    const e = this.expression.explain(cxt)
    const d = v.concat(e)
-   d.push(new Step(cxt.id++, '[def ' + last(v).id + ' ' + last(e).id + ']\t' +
-      show.typing(this.typing) + '\n'))
+   d.push(new Step(cxt.id++, '[def ' + last(v).id + ' ' + last(e).id + ']',
+      show.typing(this.typing)))
    return d 
 }
 
@@ -73,28 +91,23 @@ AST.Assignment.prototype.generate = function(indent) {
 
 AST.Return.prototype.explain = function(cxt) {
    const e = this.expression.explain(cxt)
-   e.push(new Step(cxt.id++, '[rtn ' + last(e).id + ']\t' + show.typing(this.typing) + '\n'))
+   e.push(new Step(cxt.id++, '[rtn ' + last(e).id + ']', show.typing(this.typing)))
    return e
 }
 
 AST.Block.prototype.explain = function(cxt) {
    const s = this.statements.map((x) => x.explain(cxt))
-   const b = [].concat.apply([], s)
-   b.push(new Step(cxt.id++, '[blk ' + s.map((x) => last(x).id).join(' ') + ']\t' +
-      show.typing(this.typing) + '\n'))
+   const b = flatten(s)
+   b.push(new Step(cxt.id++, '[blk ' + s.map((x) => last(x).id).join(' ') + ']',
+      show.typing(this.typing)))
    return b
 }
 
+//----------------------------------------------------------------------------
 
 return (ast) => {
-   const lines = ast.explain({
-      id : 1
-   })
-   let str = ''
-   for (let x of lines) {
-      str += x.id + '. ' + x.text
-   }
-   return str
+   const lines = ast.explain({id : 1})
+   return lines.map((x) => x.id + '. ' + x.node + '\t' + x.type + '\n').join('')
 }
 
 })()
