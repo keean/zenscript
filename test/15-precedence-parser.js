ee82095b2cb92b57e10305826bef48e6fbd77eb1
Keean Schupke <keean@fry-it.com>
2016-11-14 18:09:03 +0000

    add test cases for negate and power combinations

--- test/15-precedence-parser.js
+++ test/15-precedence-parser.js
@@ -7,7 +7,7 @@ const PP = require('../src/parse-precedence.js')
 const hspace = P.regex(/[ ]*/)
 function token(tok) {return tok.skip(hspace)}
 
-const oper = token(P.regex(/[-+*/~]+/))
+const oper = token(P.regex(/[-+*/~^]+/))
 const term = token(P.regex(/[0-9]+/).map((i) => {return parseInt(i)}))
 
 const PPP = new PP.PrecedenceParser(oper, term)
@@ -36,11 +36,17 @@ PPP.ledop('/', PP.lAssoc, 70, function(info, lhs) {
    })
 })
 
+PPP.ledop('^', PP.lAssoc, 90, function(info, lhs) {
+   return this.parseExprWithMinimumPrecedence(info.minimumPrecedence()).map((rhs) => {
+      return {'pow' : [lhs, rhs]}
+   })
+})
+
 PPP.ledop('++', null, 90, function(info, lhs) {
    return P.succeed({'post-inc' : [lhs]})
 })
 
-PPP.nudop('~', 30, function(info) {
+PPP.nudop('-', 80, function(info) {
    return this.parseExprWithMinimumPrecedence(info.minimumPrecedence()).map((rhs) => {
       return {'neg' : [rhs]}
    })
@@ -48,9 +54,28 @@ PPP.nudop('~', 30, function(info) {
 
 describe('Precedence Parser', () => {
    it('parse operator expression', () => {
-      expect(PPP.parseExprWithMinimumPrecedence(0).parse('8 + 8 * ~2++ - 1')).to.deep.equal({
+      expect(PPP.parseExprWithMinimumPrecedence(0).parse('8 + 8 * -2++ - 1')).to.deep.equal({
          "status": true, "value": {
-            "add" : [8, {"mul" : [8, {"neg" :  [{"sub" : [{"post-inc" : [2]}, 1]}]}]}]
+            "sub": [{"add": [8, {"mul" : [8, {"neg" : [{"post-inc" : [2]}]}]}]}, 1]
+         }
+      })
+   })
+
+   it('parse operator expression', () => {
+      expect(PPP.parseExprWithMinimumPrecedence(0).parse('-3 ^ 2')).to.deep.equal({
+         "status": true, "value": {
+            "neg": [{
+               "pow": [3, 2]
+            }]
+         }
+
+      })
+   })
+
+   it('parse operator expression', () => {
+      expect(PPP.parseExprWithMinimumPrecedence(0).parse('3 ^ -2')).to.deep.equal({
+         "status": true, "value": {
+            "pow": [3, {"neg": [2]}]
          }
       })
    })
